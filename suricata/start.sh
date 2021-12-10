f9f30517ec5e30144f27f5abaaa14779a3a5a1db
huangyingting <huangyingting@outlook.com>
2021-12-10 11:34:25 +0800

    Update suricata script

---
+++ suricata/start.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/dumb-init /bin/sh
+set -e
+
+update_suricata_rules() {
+	echo "Update suricata rules"
+    suricata-update
+    crond -b
+    crontab /etc/crontabs/suricata-update-cron
+}
+
+start_suricata()
+{    
+    if [ "${1#-}" != "$1" ]; then
+        set -- suricata "$@"
+    elif [ "$1" = 'suricata' ]; then
+        shift
+        set -- suricata "$@"
+    fi
+    echo "Start suricata with command line: $@"
+    exec "$@"
+}
+
+update_suricata_rules
+start_suricata "$@"
